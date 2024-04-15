d2c04bae45a0607732650086e3ade6ec5a55abe3
Marcus <ahmed.mehdi.2009@gmail.com>
2024-04-15 19:33:15 +0100

    youtube code with john

--- src/main/java/com/mytutorials/javacore/exceptions/codingwithjohn/CheckedUncheckedMain.java
+++ src/main/java/com/mytutorials/javacore/exceptions/codingwithjohn/CheckedUncheckedMain.java
@@ -2,20 +2,31 @@ package com.mytutorials.javacore.exceptions.codingwithjohn;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class CheckedUncheckedMain {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        readFile("readme.txt"); // checked Exception
+        // readFile("readme.txt"); // checked Exception
 
-        String name = null;
-        System.out.println(name.length()); // NPE
+        // String name = null;
+        // System.out.println(name.length()); // NPE
 
+        System.out.println(parseFile("/home/fake-path/")); // David
     }
 
     private static void readFile(String fileName) throws FileNotFoundException {
         FileReader fileReader = new FileReader(fileName);
+    }
 
+
+    private static String parseFile(String filePath) {
+        String name = "David";
+        try {
+            throw new IOException();
+        } finally {
+            return name;
+        }
     }
 }
